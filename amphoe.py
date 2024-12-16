4b33dd43c72f02a6f137632ca15838fc680fac08
Siraphop <6710301036@cdti.ac.th>
2024-12-16 20:51:30 +0700

    add readfile amphoe

--- amphoe.py
+++ amphoe.py
@@ -9,7 +9,7 @@ def filter_columns(values, keep_columns):
     return filtered
 
 # ใช้ with เพื่อเปิดไฟล์
-with open("amphose.sql", "r", encoding="utf-8") as sql_file, open("amphose.txt", "w", encoding="utf-8") as txt_file:
+with open("amphoe.sql", "r", encoding="utf-8") as sql_file, open("amphoe.txt", "w", encoding="utf-8") as txt_file:
     for line in sql_file:
         # ใช้ regex ดึงเฉพาะข้อมูลใน VALUES(...)
         match = re.search(r"VALUES\s*\((.+)\);", line)
@@ -17,15 +17,15 @@ with open("amphose.sql", "r", encoding="utf-8") as sql_file, open("amphose.txt",
             # ดึงค่าที่อยู่ในวงเล็บ VALUES(...)
             values = match.group(1)
             
-            # เลือกเฉพาะคอลัมน์ pcode (0) และ pname (1)
+            # เลือกเฉพาะคอลัมน์ mcode (0) และ mname (1)
             keep_columns = [0, 1]  # คอลัมน์ที่ต้องการ
             filtered_values = filter_columns(values, keep_columns)
             
-            # ลบ ' ออกจาก pcode และคงเลขศูนย์ไว้
-            pcode = filtered_values[0].replace("'", "")
+            # ลบ ' ออกจาก mcode และคงเลขศูนย์ไว้
+            acode = filtered_values[0].replace("'", "")
             
-            # ลบ ' ออกจาก pname
-            pname = filtered_values[1].replace("'", "")
+            # ลบ ' ออกจาก mname
+            aname = filtered_values[1].replace("'", "")
             
             # เขียนค่าที่ทำความสะอาดแล้วลงไฟล์ .txt
             txt_file.write(f"{acode} {aname}\n")
